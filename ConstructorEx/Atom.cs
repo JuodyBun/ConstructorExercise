347d38c5ae7d47040017ffda8e7ee5efc1f2a4af
Juody Bun <bjuody@gmail.com>
2020-01-24 10:47:11 -0600

    Add project files.

---
+++ ConstructorEx/Atom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorEx
+{
+    public class Atom
+    {
+        public int atomProtons; //properties
+        public int atomElectrons; //properties
+
+        public Atom()
+        {
+            //default control
+        }
+        public Atom(int electrons, int protons)
+        {
+            Protons = protons;
+            Electrons = electrons;
+        }
+
+        public int Protons {
+            get => atomProtons; 
+            set => atomProtons = value; 
+        }
+        
+        public int Electrons {
+            get => atomElectrons; 
+            set => atomElectrons = value; 
+        }
+
+        ~Atom()
+        {
+            Console.WriteLine("Goodbye");
+            Console.WriteLine($"Current number of protons: {atomProtons}");
+            Console.WriteLine($"Current number of electrons: {atomElectrons}");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ConstructorEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConstructorEx
+{
+    class Program
+    {
+        static Random rng = new Random();
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            MakeAtom();
+            Console.WriteLine("More code.");
+            MakeAtom();
+            Console.WriteLine("Even more code.");
+            Console.WriteLine();
+            GC.Collect();
+        }
+
+        private static void MakeAtom()
+        {
+            _ = new Atom(rng.Next(16), rng.Next(10));
+        }
+    }
+}
